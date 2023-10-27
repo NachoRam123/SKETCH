59abb5fe57e6c4b5c09fbffa7c16fd9407f89c2d
NachoRam123 <nacho_ramella@hotmail.com>
2023-10-26 22:41:23 -0300

    create input function on click buton

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
+let number = 10;
+
+let changeNumber = document.querySelector('button');
+changeNumber.addEventListener('click', inputNumber);
+
+
 let gridContainer = document.querySelector('#grid_container');
-for (let i = 1; i<=16; i++) {
+for (let i = 1; i<=number; i++) {
     let divVertical = document.createElement('div');
     divVertical.classList.add('div_squares', 'verticals');
     gridContainer.appendChild(divVertical);
-    for (let i = 1; i<=16; i++) {
+    for (let i = 1; i<=number; i++) {
         let divHoriz = document.createElement('div');
         divHoriz.classList.add('div_squares', 'horizontals');
         divVertical.appendChild(divHoriz);
     }    
 }
 
-console.log(gridContainer)
+function inputNumber() {
+    let error = true
+    while (error == true) {
+        newNumber = parseInt(prompt('Set the Number of squares', '16'));
+        if (isNaN(newNumber))  {
+            alert('Put a number please');
+        }
+        else if (newNumber > 50) {
+            alert('Put a lower number please')
+        }
+        else if (newNumber < 0) {
+            alert('Put a positive number please')
+        }
+        else {
+            error = false
+        }
+    }
+    number = newNumber;
+}
